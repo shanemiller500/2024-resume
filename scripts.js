72aaaf6a0e49a69efb43afa7ba1f6cab3c7e26b0
shanemiller500 <shanemiller500@gmail.com>
2024-02-04 21:11:42 -0700

    updated scripts

--- scripts.js
+++ scripts.js
@@ -3,3 +3,11 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
 gtag('config', 'G-CC1SCRJJSW');
+
+var trackOutboundLink = function(url) {
+    ga('send', 'event', 'outbound', 'click', url, {
+      'transport': 'beacon',
+      'hitCallback': function(){window.open(url);}
+    });
+ }
+ 
