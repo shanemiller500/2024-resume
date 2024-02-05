a8db0decb103f5e6d1c771a7a7448f0a363eea0b
shanemiller500 <shanemiller500@gmail.com>
2024-02-04 21:14:03 -0700

    updated scripts

--- scripts.js
+++ scripts.js
@@ -1,13 +1,24 @@
+(function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date(); a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-91612823-1', 'auto');
+ga('send', 'pageview');
+
+
+
 window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
+function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
 
 gtag('config', 'G-CC1SCRJJSW');
 
-var trackOutboundLink = function(url) {
+var trackOutboundLink = function (url) {
     ga('send', 'event', 'outbound', 'click', url, {
-      'transport': 'beacon',
-      'hitCallback': function(){window.open(url);}
+        'transport': 'beacon',
+        'hitCallback': function () { window.open(url); }
     });
- }
- 
+}
