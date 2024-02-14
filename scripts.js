4f099c587224f022f055bf819cfc8ec2a4b3c5b2
shanemiller500 <shanemiller500@gmail.com>
2024-02-13 21:34:47 -0700

    removed old GA

--- scripts.js
+++ scripts.js
@@ -8,21 +8,12 @@
 ga('create', 'UA-91612823-1', 'auto');
 ga('send', 'pageview');
 
-
-
-window.dataLayer = window.dataLayer || [];
-function gtag() { dataLayer.push(arguments); }
-gtag('js', new Date());
-
-gtag('config', 'G-CC1SCRJJSW');
-
 var trackOutboundLink = function (url) {
     ga('send', 'event', 'outbound', 'click', url, {
         'transport': 'beacon',
         'hitCallback': function () { window.open(url); }
     });
 }
-
 
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
